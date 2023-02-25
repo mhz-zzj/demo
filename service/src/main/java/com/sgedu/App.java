013c461c3dbbcf1106da146df28be715be3e41e8
mhzzj <3200846150@qq.com>
2023-02-25 15:59:21 +0800

    demo

---
+++ service/src/main/java/com/sgedu/App.java
@@ -0,0 +1,14 @@
+package com.sgedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//mo19128056972
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class);
+    }
+
+}
